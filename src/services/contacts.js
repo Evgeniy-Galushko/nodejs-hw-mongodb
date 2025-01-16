50e93271e86135fc7b1a734d1ab297cc885f8002
Galushko Evgeniy <yudginchik@gmail.com>
2025-01-16 12:20:28 +0200

    add

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,6 +6,45 @@ export const allContacts = async () => {
 };
 
 export const contactById = async (contactId) => {
-  const contact = await СontactsCollection.findById(contactId);
+  try {
+    const contact = await СontactsCollection.findById(contactId);
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const additionContact = async (payload) => {
+  const contact = await СontactsCollection.create(payload);
   return contact;
+};
+
+export const deleteContact = async (contactId) => {
+  try {
+    const contact = await СontactsCollection.findOneAndDelete({
+      _id: contactId,
+    });
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const updatedСontact = await СontactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!updatedСontact || !updatedСontact.value) return null;
+
+  return {
+    contact: updatedСontact.value,
+    isNew: Boolean(updatedСontact?.lastErrorObject.upserted),
+  };
 };
